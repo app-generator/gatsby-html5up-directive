e22dca4f9561f27711455b6543f4751762e9ad42
Adi Chirilov <developer@rosoftware.ro>
2019-08-08 11:46:45 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <div id="header">
       <span className="logo icon fa-paper-plane-o"></span>
-      <h1>Gatsby Html5up</h1>
+      <h1>
+      <a href="https://www.gatsbyjs.org/">Gatsby</a> Html5up
+      </h1>
       <p><a href="https://github.com/app-generator/gatsby-html5up-directive">Open-Source</a> app with Directive design</p>
     </div>
   );
